59093ab4b3c83cf1a6e0e6347b7cc50a6441608e
safwanaec <ashraisafwan@gmail.com>
2025-02-05 02:05:49 +0600

    more fixes

---
+++ Algorithm/test.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class test {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int t = inp.nextInt();
+        int n,k;
+        while(t-->0){
+            n = inp.nextInt();
+            k=inp.nextInt();
+        }
+        out.close();
+        inp.close();
+    }
+}
