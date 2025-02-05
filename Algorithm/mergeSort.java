5d51540c4253b3b4db33e1d4954eda9f79563251
safwanaec <ashraisafwan@gmail.com>
2025-02-05 19:45:11 +0600

    testng

--- Algorithm/mergeSort.java
+++ Algorithm/mergeSort.java
@@ -2,32 +2,32 @@ import java.util.Arrays;
 public class mergeSort{
     public static void main(String[] args) {
         int[] arr = {1,8,8,2,8,6,8,7,1,4,6,2,5,44,6,99};
-        arr = mergesort(arr, arr.length);
+        arr = mergesort(arr);
         for(int i=0; i<arr.length; i++){
             System.out.println(arr[i]);
         }
     }
-    static int[] mergesort(int[] Array, int right) {
-        int mid = (right) / 2;
-        if (0 >= right - 1) {
+    static int[] mergesort(int[] Array) {
+        int mid = (Array.length) / 2;
+        if (Array.length<=1) {
             return Array;
         } else {
-            int x = 0, y = 0, z = 0;
-            int[] n1 = mergesort(Arrays.copyOfRange(Array, 0, mid),mid);
-            int[] n2 = mergesort(Arrays.copyOfRange(Array, mid, Array.length),right-mid);
+            int leftArrayIndex = 0, rightArayIndex = 0, arrayIndex = 0;
+            int[] leftArray = mergesort(Arrays.copyOfRange(Array, 0, mid));
+            int[] rightArray = mergesort(Arrays.copyOfRange(Array, mid, Array.length));
 
-            while (x < n1.length && y < n2.length) {
-                if (n1[x] > n2[y]) {
-                    Array[z] = n2[y];
-                    y++;
+            while (leftArrayIndex < leftArray.length && rightArayIndex < rightArray.length) {
+                if (leftArray[leftArrayIndex] > rightArray[rightArayIndex]) {
+                    Array[arrayIndex] = rightArray[rightArayIndex];
+                    rightArayIndex++;
                 } else {
-                    Array[z] = n1[x];
-                    x++;
+                    Array[arrayIndex] = leftArray[leftArrayIndex];
+                    leftArrayIndex++;
                 }
-                z++;
+                arrayIndex++;
             }
-            while(x<n1.length)Array[z++]=n1[x++];
-            while(y<n2.length)Array[z++]=n2[y++];
+            while(leftArrayIndex<leftArray.length)Array[arrayIndex++]=leftArray[leftArrayIndex++];
+            while(rightArayIndex<rightArray.length)Array[arrayIndex++]=rightArray[rightArayIndex++];
         }
         return Array;
     }
